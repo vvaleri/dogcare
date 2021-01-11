8b53d55167d824f1940d91bd0e1faa83d72686fe
Valeria Rodionova <valeriya_off@mail.ru>
2021-01-12 00:54:17 +0300

    Add AOS. Scroll animation

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+AOS.init({
+    duration: 1000,
+});
+
 const tabs = document.querySelectorAll('.galery__item'),
     content = document.querySelectorAll('.galery__img'),
     container = document.querySelector('.galery__tabs'),
